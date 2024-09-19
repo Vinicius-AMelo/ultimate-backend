621a07c70db00488ddecc912d4f40d4605d9429a
Vinicius Melo <vinicius.apol.melo@gmail.com>
2024-09-19 17:24:12 -0300

    :sparkles: feat: dockerfile wip

--- services/api/cmd/main.go
+++ services/api/cmd/main.go
@@ -2,33 +2,41 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/rabbitmq/amqp091-go"
 )
 
 type session struct {
-	ID string `json:"id"`
+	ID    string `json:"id"`
 	Token string `json:"token"`
 }
 
-func main(){
+func main() {
+	gin.SetMode(gin.ReleaseMode)
 	r := gin.Default()
 
-	conn, err := amqp091.Dial("amqp://guest:guest@localhost:5672/");
-	if err != nil{
-		panic(err)
-	} 
-	defer conn.Close()
+	// conn, err := amqp091.Dial("amqp://guest:guest@localhost:5672/");
+	// if err != nil{
+	// 	panic(err)
+	// }
+	// defer conn.Close()
 
-	ch, err := conn.Channel()
-	if err != nil {
-		panic(err)
-	}
-	defer ch.Close()
+	// ch, err := conn.Channel()
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// defer ch.Close()
 
 	r.POST("/session", func(ctx *gin.Context) {
 		var body session
-		err := ctx.BindJSON(&body); if err != nil {
+		err := ctx.BindJSON(&body)
+		if err != nil {
+			ctx.JSON(400, gin.H{"error": err.Error()})
 			return
 		}
+
+		ctx.JSON(200, gin.H{"id": body.ID, "token": body.Token})
 	})
+
+	if err := r.Run(":8080"); err != nil {
+		panic(err)
+	}
 }
